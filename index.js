4ed8b218edda3d7bfd35ea2828367758a4b6dbe7
joseph <josephtkao@gmail.com>
2021-07-02 14:05:42 -0500

    updated functions to pass tests

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, adj) {
+    let answer =[]
+    for (let i = 0; i < names.length; i++){
+        console.log(`Thank you, ${names[i]}, for the wonderful ${adj} gift!`)
+        answer.push(`Thank you, ${names[i]}, for the wonderful ${adj} gift!`)
+    }
+    return answer
+}
+
+function countDown(number) {
+    for (let i = number; i>=0; i--){
+        console.log(i)
+    }
+}
+
